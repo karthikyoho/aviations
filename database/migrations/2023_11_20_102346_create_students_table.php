6757316f6c17155d56e8dad6ed05399fd5b06d57
Santhosh-Raiser <santhoshsv358@gmail.com>
2023-11-20 16:59:04 +0530

    santhosh commit

--- database/migrations/2023_11_20_102346_create_students_table.php
+++ database/migrations/2023_11_20_102346_create_students_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('student_id')->unique();
+            $table->unsignedBigInteger('user_id');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('father_name');
@@ -39,6 +40,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('is_active',['yes','no'])->default('yes');
             $table->enum('is_deleted',['yes','no'])->default('no');
+            $table->foreign('user_id')->references('id')->on('users');
+           
              $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
