2024f842f1f3a721bba2c211051b53af91907249
karthik5498 <142414299+karthik5498@users.noreply.github.com>
2023-11-20 19:52:33 +0530

    karthik commit

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Student extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    
+       
+    
+        
+        public static function boot()
+        {
+            parent::boot();
+    
+            static::creating(function ($student) {
+                if (!$student->college_id) {
+                    $student->student_id  = self::generateUniqueCollegeId(strtoupper($student->first_name));
+                }
+            });
+        }
+    
+        // Generate a unique 'college_id'
+        public static function generateUniqueCollegeId($name)
+        {
+            $studentPrefix = Str::slug($name) . "01";
+            $studentPrefix = strtoupper($studentPrefix);
+            return $studentPrefix;
+        }
+    
+    
+
+
+
+
+
+
     
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Student\Authentication\AuthenticationController;
+use App\Http\Controllers\Student\CollegeManagemet\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register',[AuthenticationController::class,'register']);
 
+
+
+Route::post('create',[StudentController::class,'createUser']);
