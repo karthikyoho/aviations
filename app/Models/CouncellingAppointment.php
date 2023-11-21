3de136bdd58c8af46774d6b4ec1c9b862f6bbafa
Santhosh-Raiser <santhoshsv358@gmail.com>
2023-11-21 13:19:25 +0530

    santhosh commit

---
+++ app/Models/CouncellingAppointment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CouncellingAppointment extends Model
+{
+    use HasFactory;
+}
